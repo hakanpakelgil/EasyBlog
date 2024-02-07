e157900e3696c9a89b645ea7fee9e3ea19416d2e
hakanpakelgil <hakanpakelgil@hotmail.com>
2024-02-07 12:46:38 +0300

    listing

---
+++ EasyBlog/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using EasyBlog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBlog.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {          
+            return View(_db.Categories.ToList());
+        }
+    }
+}
